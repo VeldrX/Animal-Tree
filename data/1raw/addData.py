a4e6a6889df6896648844cc7176ffa1083ee6593
VeldrX <vassymathis8@gmail.com>
2025-01-21 14:54:54 +0100

    delete file

--- data/1raw/addData.py
+++
@@ -1,7 +0,0 @@
-import kagglehub
-
-# Specify the directory where you want to save the 
-# Download the dataset to the specified path
-path = kagglehub.dataset_download("willianoliveiragibin/animal-analyzing")
-
-print("Path to dataset files:", path)
